9513f0f0d08fdb3f8562cab64058e385eed60991
Abrahamjm8 <abraham126@revature.net>
2023-01-18 13:23:54 -0800

    updated terraform files

---
+++ Terraform/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+ 
+  # backend "s3" {
+  #   bucket = "teamkuberknights-bucket-01"
+  #   profile = "Abraham"
+  #   region = "us-east-1"
+  #   key = "state/terraform.tfstate"
+  #   encrypt = true
+  # }
+}
